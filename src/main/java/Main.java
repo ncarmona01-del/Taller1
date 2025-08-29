abf2de3f5ce6f7bb7fa08097d3422fe1dba79b63
Taller <n.carmona01@ufromail.cl>
2025-08-29 10:43:15 -0400

    corregido

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,31 @@
-public static class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main() {
+
     }
+
+    public static void MostrarMenu() {
+        System.out.println("Menu Principal");
+        System.out.println("1. Verificar si una Frase es Revés-Derecho");
+        System.out.println("2. Contar vocales en una frase");
+        System.out.println("3. Encriptar una Frase");
+        System.out.println("4. Desencriptar Frase");
+        System.out.println("5. Salir ");
+    }
+
+    public static void IngresarFrase(Scanner sc) {
+        System.out.println("Ingresar Frase");
+        int frase = sc.nextInt();
+    }
+
+    public static void VerificarFrase() {
+
+    }
+
+
+
+
+
 }
-void main() {
-}
-
-public static void MostrarMenu() {
-    System.out.println("Menu Principal");
-    System.out.println("1. Verificar si una Frase es Revés-Derecho");
-    System.out.println("2. Contar vocales en una frase");
-    System.out.println("3. Encriptar una Frase");
-    System.out.println("4. Desencriptar Frase");
-    System.out.println("5. Salir ");
-}
-
-
-
