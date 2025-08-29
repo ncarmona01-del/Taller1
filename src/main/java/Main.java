c128cfb5704b0a7e207d4781677705885b269e46
Taller <n.carmona01@ufromail.cl>
2025-08-29 10:21:17 -0400

    menu

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public static class Main {
+    public static void main(String[] args) {
+    }
+}
+void main() {
+}
+
+public static void MostrarMenu() {
+    System.out.println("Menu Principal");
+    System.out.println("1. Verificar si una Frase es Revés-Derecho");
+    System.out.println("2. Contar vocales en una frase");
+    System.out.println("3. Encriptar una Frase");
+    System.out.println("4. Desencriptar Frase");
+    System.out.println("5. Salir ");
+}
+
+
+
